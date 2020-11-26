a8f0aaf991cd8645f15d87f6a33391b71e9921bc
Joy Christine <joychristin2@gmail.com>
2020-11-26 12:20:42 +0300

    added port 3000

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("we are happy")
+import Express from "express";
+
+const app = Express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("thththt");
+
+});
+app.listen(port, () => console.log("listening on port " + port))
